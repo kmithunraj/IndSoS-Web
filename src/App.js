4e312bcaeb97613c536c1593d0d76ea31b6ecaaa
Mithun Raj <kmithunraj02@gmail.com>
2023-10-07 15:17:09 +0530

    Revert "v1"
    This reverts commit e2d0120dc34d8cf7ff83961a8cf796367f9b3ed6.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar.jsx';
 import Alertbar from './components/Alertbar.jsx';
 import Dashboard from './components/Dashboard.jsx';
@@ -9,43 +9,37 @@ import Resources from './components/Resources';
 import Analytics from './components/Analytics';
 import Settings from './components/Settings';
 import Search from './components/Search';
-import Login from './components/Login';
+
+
 
 function App() {
 
-  const authenticated = true;
-  
+
+
+
+
   return (
     <div className="App">
+
       <BrowserRouter>
-      {authenticated && (
-          <>
-            <Sidebar />
-            <Alertbar />
-          </>
-        )}
+
+        <Sidebar />
+        <Alertbar />
+
         <Routes>
-          {authenticated ? (
-            <>
-              <Route path="*" element={<Navigate to="/Dashboard" />} />
-              <Route path="/" element={<Navigate to="/Dashboard" />} />
-              <Route path="/Login" element={<Navigate to="/Dashboard" />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-              <Route path="/Search" element={<Search />} />
-              <Route path="/ManageTeam" element={<ManageTeam />} />
-              <Route path="/Statement" element={<Statement />} />
-              <Route path="/Resources" element={<Resources />} />
-              <Route path="/Analytics" element={<Analytics />} />
-              <Route path="/Settings" element={<Settings />} />
-            </>
-          ) : (
-            <>
-              <Route path="/*" element={<Navigate to="/login" />} />
-              <Route path="/login" element={<Login />} />
-            </>
-          )}
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Search" element={<Search />} />
+          <Route path="/ManageTeam" element={<ManageTeam />} />
+          <Route path="/Statement" element={<Statement />} />
+          <Route path="/Resources" element={<Resources />} />
+          <Route path="/Analytics" element={<Analytics />} />
+          <Route path="/Settings" element={<Settings />} />
         </Routes>
+
+        
+
       </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Alertbar.jsx
+++ src/components/Alertbar.jsx
@@ -4,8 +4,6 @@ import "../App.css";
 
 const Alertbar = () => {
 
-    const longitude = 72.8333;
-    const latitude = 18.9217;
 
     return (
 
@@ -20,12 +18,6 @@ const Alertbar = () => {
             </div>
 
             <div className="alertmapWrapper">
-
-                <div id="map">
-
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.6935249740864!2d77.58884117863154!3d12.991443554646015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1646db287a35%3A0xa12a9986d54a928b!2sDesserted%20Caf%C3%A9%20%26%20Patisserie!5e0!3m2!1sen!2sin!4v1696175417394!5m2!1sen!2sin" width="320" height="305" style={{border:'0', margin:'5px'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-                </div>
 
             </div>
 
