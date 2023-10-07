740221650f5880da1653577e65c429a526022b74
Mithun Raj <kmithunraj02@gmail.com>
2023-10-07 13:24:28 +0530

    Auto stash before merge of "main" and "origin/main"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar.jsx';
 import Alertbar from './components/Alertbar.jsx';
 import Dashboard from './components/Dashboard.jsx';
@@ -9,37 +9,43 @@ import Resources from './components/Resources';
 import Analytics from './components/Analytics';
 import Settings from './components/Settings';
 import Search from './components/Search';
-
-
+import Login from './components/Login';
 
 function App() {
 
-
-
-
-
+  const authenticated = true;
+  
   return (
     <div className="App">
-
       <BrowserRouter>
-
-        <Sidebar />
-        <Alertbar />
-
+      {authenticated && (
+          <>
+            <Sidebar />
+            <Alertbar />
+          </>
+        )}
         <Routes>
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Search" element={<Search />} />
-          <Route path="/ManageTeam" element={<ManageTeam />} />
-          <Route path="/Statement" element={<Statement />} />
-          <Route path="/Resources" element={<Resources />} />
-          <Route path="/Analytics" element={<Analytics />} />
-          <Route path="/Settings" element={<Settings />} />
+          {authenticated ? (
+            <>
+              <Route path="*" element={<Navigate to="/Dashboard" />} />
+              <Route path="/" element={<Navigate to="/Dashboard" />} />
+              <Route path="/Login" element={<Navigate to="/Dashboard" />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Search" element={<Search />} />
+              <Route path="/ManageTeam" element={<ManageTeam />} />
+              <Route path="/Statement" element={<Statement />} />
+              <Route path="/Resources" element={<Resources />} />
+              <Route path="/Analytics" element={<Analytics />} />
+              <Route path="/Settings" element={<Settings />} />
+            </>
+          ) : (
+            <>
+              <Route path="/*" element={<Navigate to="/login" />} />
+              <Route path="/login" element={<Login />} />
+            </>
+          )}
         </Routes>
-
-        
-
       </BrowserRouter>
-
     </div>
   );
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 
 const Dashboard = () => {
 
+
     return (
 
         <div className="pageWrapper">
@@ -22,6 +23,46 @@ const Dashboard = () => {
 
             <div className="box">
 
+
+            </div>
+
+            </div>
+
+            <div className="activeAlerts">
+                <h1>Active Alerts</h1>
+                <hr className="line" />
+
+                <div className="activeAlertWrapper">
+
+                <div className="activeAlert">
+
+                    <div className="redBox"></div>
+
+                        <div className="detailsHeader">
+                            <h3>Name:</h3>
+                            <h3>From:</h3>
+                            <h3>Location:</h3>
+                            <h3 className="detailsAddress">Address:</h3>
+                            <h3>Problem:</h3>
+                            <h3>Intensity:</h3>
+                        </div>
+
+                        <div className="detailsContent">
+                            <p>K Mithun Raj</p>
+                            <p>Civilian</p>
+                            <p>13.0827° N, 80.2707° E</p>
+                            <p  className="detailsAddress">No.43, 5th Street, Bharathi Nagar, Muthapudupet, IAF, Chennai - 600055</p>
+                            <p>Flood</p>
+                            <p>High</p>
+                        </div>
+
+                        <div className="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.6935249740864!2d77.58884117863154!3d12.991443554646015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17880edac11f%3A0x20fdecf5c6eca808!2sHotels%20in%20Shimoga!5e0!3m2!1sen!2sin!4v1696174916310!5m2!1sen!2sin" width="280" height="245" style={{border:'0', margin:'5px'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                            
+</div>
+
+                </div>                
 
             </div>
 
